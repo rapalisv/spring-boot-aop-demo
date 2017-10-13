721ca3704a1d58f45fbc7c2a84cc8eca6fc97ab6
rapalisv <vitali.rapalis@msg-gillardon.de>
2017-10-13 13:13:44 +0200

    Init Project

---
+++ src/main/java/com/lito/www/aop/SpringBootAopDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lito.www.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopDemoApplication.class, args);
+	}
+}
